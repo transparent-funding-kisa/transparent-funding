385f25b54628be6b7ba43fac6d9d661ccf3412d4
이진솔 <>
2020-08-10 17:13:07 +0900

    move views folder

--- front/src/App.js
+++ front/src/App.js
@@ -1,33 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Card from "./components/commons/card.js";
-import Home from "./components/commons/homeIcon";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+import GlobalStyles from 'src/components/GlobalStyles';
+import 'src/mixins/chartjs';
+import theme from 'src/theme';
+import routes from 'src/routes';
 
-function App() {
+const App = () => {
+  const routing = useRoutes(routes);
+
   return (
-    <Router>
-      <Route path="/test" component={Card} />
-    </Router>
-    // <Home></Home>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {routing}
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
