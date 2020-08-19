3895b76500990e94204ee1eaf05dacbc6ada4462
HyeonHak <52483468+HyeonHak@users.noreply.github.com>
2020-08-19 15:33:03 +0900

    충돌해결2

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,20 @@
 // import "react-perfect-scrollbar/dist/css/styles.css";
-import React from 'react';
-import { useRoutes } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/core';
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core";
+import Signup from './views/SignUp';
+import Signin from './views/SignIn';
+import Mypage from './views/MyPage';
+
+
 // import GlobalStyles from "src/components/GlobalStyles";
 // import "src/mixins/chartjs";
 // import theme from "src/theme";
 // import routes from "src/routes";
 
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
-import Test from './views/Test';
-import FundingApply from './views/FundingApply/FundingApply';
-import Detail from './views/DetailPage/Detail';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Test from "./views/Test";
+import FundingApply from "./views/FundingApply";
 
 const App = () => {
   // const routing = useRoutes(routes);
@@ -19,7 +23,14 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Route path="/FundingApply/:Id" exact component={FundingApply}></Route>
-        <Route path="/Detail/:Id" exact component={Detail}></Route>
+        <Route path="/Signup" exact component={Signup}></Route>
+        <Route path="/Signin" exact component={Signin}></Route>
+        <Route path="/MyPage" exact component={Mypage}></Route>
+
+        
+        
+        
+
         {/* <Route path="/1" component={Test}></Route> */}
       </Switch>
     </BrowserRouter>
