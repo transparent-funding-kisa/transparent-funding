801d9851d238014caf335f2410db61b9ac860a7d
HyeonHak <52483468+HyeonHak@users.noreply.github.com>
2020-08-19 15:57:16 +0900

    detail 연동

--- front/src/views/FundingApply/FormDialog.js
+++ front/src/views/FundingApply/FormDialog.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Text from '../../components/commons/textField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Select from '../../components/commons/select';
 
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
@@ -35,6 +37,7 @@ export default function FormDialog() {
             서포터에게 제공하는 리워드의 종류를 선택하고 해당하는 리워드 정보
             제공 고시를 입력하세요.
           </DialogContentText>
+          <Select />
           <TextField
             autoFocus
             margin="dense"
@@ -43,6 +46,20 @@ export default function FormDialog() {
             type="email"
             fullWidth
           />
+          {/* <Text></Text> */}
+          품명 및 모델명 <br />
+          <TextField
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+            label="test"
+          />
+          품명 및 모델명 <br />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          품명 및 모델명 <br />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          품명 및 모델명 <br />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
